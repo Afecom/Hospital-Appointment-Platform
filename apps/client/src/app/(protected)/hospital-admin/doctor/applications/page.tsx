2b7ffad3b11ff2218f9c31dc9c2da3ee24b3cb0a
Nuru Kamil <knuru472@gmail.com>
2026-01-11 20:53:45 +0300

    clean up

--- apps/client/src/app/(protected)/hospital-admin/doctor/applications/page.tsx
+++ apps/client/src/app/(protected)/hospital-admin/doctor/applications/page.tsx
@@ -94,11 +94,11 @@ const DoctorApplicationsPage = () => {
                   </div>
                 </div>
                 <div className="flex justify-end gap-3 mt-4">
-                  <button className="px-4 py-2 rounded-md bg-red-600 text-white font-semibold hover:bg-red-800 hover:cursor-pointer transition-colors">
-                    Reject
-                  </button>
                   <button className="px-4 py-2 rounded-md bg-secondary text-white font-semibold hover:bg-blue-950 hover:cursor-pointer transition-colors">
                     Approve
+                  </button>
+                  <button className="px-4 py-2 rounded-md bg-red-600 text-white font-semibold hover:bg-red-800 hover:cursor-pointer transition-colors">
+                    Reject
                   </button>
                 </div>
               </div>
