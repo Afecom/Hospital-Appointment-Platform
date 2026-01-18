a875faaceb51e3ef3c66b28010d0808fdee9276a
Nuru Kamil <knuru472@gmail.com>
2026-01-18 22:08:46 +0300

    completed type definations for hospital admin doctors page API requests

--- packages/contract/src/schemas/doctor.schema.ts
+++ packages/contract/src/schemas/doctor.schema.ts
@@ -7,3 +7,26 @@ export interface countHospitalDoctorsRes {
   message: string;
   total: number;
 }
+
+export interface inactiveHospitalDoctorsRes {
+  inactiveDoctors: {
+    id: string;
+    Doctor: {
+      id: string;
+      User: {
+        id: string;
+        fullName: string;
+        phoneNumber: string | null;
+      };
+    };
+    slotDuration: number;
+  };
+  meta: {
+    total: number;
+    page: number;
+    limit: number;
+    lastPage: number;
+    hasNextPage: boolean;
+    hasPrevPage: boolean;
+  };
+}
